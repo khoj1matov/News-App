8e9c68e55bb2949da036394c56c5ac98a2e32f9e
khoj1matov <azamjonxojimatov6027@gmail.com>
2022-08-06 20:10:38 +0500

    commit

--- lib/core/constants/api_url_const.dart
+++ lib/core/constants/api_url_const.dart
@@ -1,8 +1,8 @@
 class ApiUrlConst {
   static String appleApi =
-      "https://newsapi.org/v2/everything?q=apple&from=2022-07-28&to=2022-07-28&sortBy=popularity&apiKey=b14ac1dba2f443a4bf1654d925d4b9a8";
+      "https://newsapi.org/v2/everything?q=apple&from=2022-08-04&to=2022-08-04&sortBy=popularity&apiKey=b14ac1dba2f443a4bf1654d925d4b9a8";
   static String teslaApi =
-      "https://newsapi.org/v2/everything?q=tesla&from=2022-06-29&sortBy=publishedAt&apiKey=b14ac1dba2f443a4bf1654d925d4b9a8";
+      "https://newsapi.org/v2/everything?q=tesla&from=2022-07-05&sortBy=publishedAt&apiKey=b14ac1dba2f443a4bf1654d925d4b9a8";
   static String businessApi =
       "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b14ac1dba2f443a4bf1654d925d4b9a8";
   static String techCrunchApi =
